15adccd72cb5ec9686a6554ecd742324487ba620
Navjot Singh <navjot_@outlook.com>
2023-04-15 00:21:45 +0530

    added links and refactoring of articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy] #run the function right before calling these functions
+
   def show
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
   end
 
   def index
@@ -12,11 +14,11 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
   end
 
   def create
-    @article = Article.new(params.require(:article).permit( :description, :title))
+    @article = Article.new(get_article_params)#works because ruby just calls it, not a func ptr
     if @article.save
       flash[:notice] = "Article was created successfully."
       #redirect_to article_path(@article)
@@ -27,13 +29,28 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:description, :title))
+    #@article = Article.find(params[:id])
+    if @article.update(get_article_params)
       flash[:notice] = "Article was created successfully!"
       redirect_to @article
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    #@article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def get_article_params
+    params.require(:article).permit(:description, :title) #works because ruby returns last line
   end
 
 end
